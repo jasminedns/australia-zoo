b527235a5493a8e8a7f8ee34cde292525bf2ab13
Moon2024-light <brite_moon2008@yahoo.com>
2024-11-26 00:50:29 +0100

    Modified mammals.js

---
+++ scripts/mammls.js
@@ -0,0 +1,27 @@
+const mammals = [
+    {
+      name: "Tasmanian Devil",
+      group: "mammals",
+      image: "\images\transparent-img\tasmanian-devil.png",
+      shortDescription: "A carnivorous marsupial found in Tasmania, known for its loud screeches...",
+      fullDescription: "The Tasmanian Devil is the world's largest surviving carnivorous marsupial. Native to Tasmania, these nocturnal animals are known for their powerful jaws and fierce temperament. Despite their reputation, they play a crucial role in maintaining ecosystem balance.",
+      diet: "Carrion, small animals, insects",
+    },
+    {
+      name: "Quokka",
+      group: "mammals",
+      image: "images/transparent-img/quokka.png",
+      shortDescription: "A small, herbivorous marsupial known as the happiest animal due to its friendly appearance...",
+      fullDescription: "The Quokka is a small wallaby native to southwestern Australia, particularly Rottnest Island. Famous for its 'smiling' expression, it is a nocturnal herbivore that feeds on grass and leaves. Quokkas are social and thrive in groups.",
+      diet: "Grasses, leaves, and roots",
+    },
+    {
+      name: "Echidna",
+      group: "mammals",
+      image: "\images\transparent-img\echidna.png",
+      shortDescription: "A spiny, egg-laying mammal, one of only two monotremes in the world...",
+      fullDescription: "The Echidna, also known as a spiny anteater, is a monotreme found in Australia and New Guinea. Known for its spines and long snout, it feeds primarily on ants and termites. Echidnas lay eggs and are solitary creatures.",
+      diet: "Ants and termites",
+    }
+  ];
+  
