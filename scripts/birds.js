80210adee36023b6f402a5ad40a24954e5205f69
Moon2024-light <brite_moon2008@yahoo.com>
2024-11-27 00:02:27 +0100

    object coding in birds js file

---
+++ scripts/birds.js
@@ -0,0 +1,63 @@
+// Cassowary Object
+const cassowary = {
+    name: "Cassowary",
+    lifespan: "20 years",
+    group: "bird",
+    image: "'\images\transparent-img\cassowary.png'",
+    food: "Plants matter like fruit, insects and small animals like mice and lizards",
+    description: `It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. 
+                  The largest cassowaries can stand as high as six feet and weigh up to 160 pounds. 
+                  These large birds cannot fly, but their extremely powerful legs propel them at great speeds. 
+                  They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and 
+                  they are usually hard to spot, at least in their natural rain forest habitats. 
+                  They are not overly aggressive, and attacks are rare. But they can do a lot of damage 
+                  if they are provoked or angered. Cassowary attacks have occasionally been deadly, including 
+                  a recent one which occurred in 2019, at a private collection of caged birds in Florida.`,
+    length: "1.7m",
+    weight: "44kg",
+    found: "Queensland"
+};
+
+// Kookaburra Object
+
+const kookaburra = {
+    name: "Cookaburra",
+    lifespan: "20 years",
+    group: "bird",
+    image: "'\images\transparent-img\cookaburra.png'",
+    food: "Insects and small animals including snakes, frogs and lizards",
+    description: `Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest 
+                  member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm 
+                  in length. Its beak can reach 10 cm long and is used to snatch a variety of invertebrates and 
+                  small vertebrates, including the occasional small snake. Since being introduced in western Australia 
+                  and New Zealand, the kookaburra has angered farmers by preying on their fowl. 
+                  The laughing kookaburra has dark brown wing plumage and a white head and underside. 
+                  Dark brown eye stripes run across its face and its upper bill is black. 
+                  Its reddish-coloured tail is patterned with black bars.`,
+    length: "43cm",
+    weight: "300g",
+    found: "Australia wide"
+};
+
+// Yellow-Tailed Black Cockatoo Object
+const yellowTailedBlackCockatoo = {
+    name: "Yellow-Tailed Black Cockatoo",
+    lifespan: "41 years",
+    group: "bird",
+    image: "'\images\transparent-img\yellow-tailed-black-cockatoo.PNG'",
+    food: "Fruit, seeds and other plant material",
+    description: `It has a short crest on the top of its head. Its plumage is mostly brownish black and 
+                  it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged 
+                  with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red 
+                  eye-rings, and the female has a bone-coloured beak and grey eye-rings. In flight, yellow-tailed 
+                  black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, 
+                  wailing calls carry for long distances. The whiteae is found south of Victoria to the East 
+                  of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in 
+                  temperate forests and forested areas across south and central eastern Queensland to 
+                  southeastern South Australia, including a very small population persisting in the Eyre Peninsula.`,
+    length: "65cm",
+    weight: "900g",
+    found: "Southeastern Australia"
+};
+
+
