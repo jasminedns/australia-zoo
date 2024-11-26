5fb56bff372fa06f806a34406856d8a32b319ffe
DenVitaKaninen <tobiaskjernell@gmail.com>
2024-11-27 00:41:15 +0100

    added auto push into object function

--- scripts/aboutus.js
+++ scripts/aboutus.js
@@ -7,25 +7,17 @@ function TeamMember(fullname, hobbies, country, topFiveFood, picture) {
     this.country = country;
     this.topFiveFood = topFiveFood;
     this.picture = picture;
+
+    allTeamMembers.push(this);
 }
 
 const createMembers = () => {
-    
-    tobiasK = new TeamMember("Tobias Kjernell", ["Music", "Spicy food", "Movies and TV-series", "Long naps"], "Sweden", ["Tonkatsu", "Wienerschnitzel with roasted potatoes", "Salmon Tikka Masala", "Creamy Chili Carbonara", "Sushi"],"'../images/team/tobias.png'");
-    allTeamMembers.push(tobiasK);
-
+    tobiasK = new TeamMember("Tobias Kjernell", ["Music", "Spicy food", "Movies and TV-series", "Long naps"], "Sweden", ["Tonkatsu", "Wienerschnitzel with roasted potatoes", "Salmon Tikka Masala", "Creamy Chili Carbonara", "Sushi"], "'../images/team/tobias.png'");
     srilathaP = new TeamMember("Srilatha Potnuru", [""], [""], "India", [""], "'../images/team/srilatha.png'");
-    allTeamMembers.push(srilathaP);
-
-    carolineL = new TeamMember("Caroline Lindbom", [""], "Sweden", [""],"'../images/team/caroline.png'");
-    allTeamMembers.push(carolineL);
-
+    carolineL = new TeamMember("Caroline Lindbom", [""], "Sweden", [""], "'../images/team/caroline.png'");
     mariymA = new TeamMember("Mariym Aqbal", [""], "Pakistan", [""], "'../images/team/mariym.png'");
-    allTeamMembers.push(mariymA);
-
     jasminD = new TeamMember("Jasmin Danese", [""], "Italy", [""], "'../images/team/jasmin.png")
-    allTeamMembers.push(jasminD);
 }
-
+console.log(allTeamMembers)
 createMembers();
 
