defab2ce1370ed808aac780ee1bd0f2eeea2f2d2
idacarlin95 <carlin95@hotmail.se>
2024-11-27 11:54:28 +0100

    Added reptile objects

---
+++ scripts/reptiles.js
@@ -0,0 +1,32 @@
+const frillNeckedLizard = {
+    lifespan: "20 years",
+    group: "Reptiles",
+    food: "Small insects and spiders",
+    length: "90cm",
+    weight: "1kg",
+    found: "Northern Australia",
+    description: "When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles it's head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run wwthout stopping or looking back until reaches the safety of a tree.",
+    shortDescription: "The frill-necked lizard is a reptile from northern Australia and New Guinea, known for its large, dramatic neck frill used to scare predators."
+};
+
+const hawksbillTurtle = {
+    lifespan: "50 years",
+    group: "Reptiles",
+    food: "other animals (sponges & jellyfish), sea plants",
+    length: "80cm (carapace)",
+    weight: "50kg",
+    found: "Tropical coasts of Queensland, Northern Territory and Western Australia",
+    description: "The Hawksbill Turtle gets its common name from the distinctive mouth, which resembales a bird's beak. The shell, or carapace, is covered in large overlapping scutes, which are distinctive brown/green/amber background with lighter brown streaks throughout. At the edge of the carapace they overlap in such a way as to form a serrate edge. The turtle has an elongated head and flippers which have two visible claws on the end. The flippers and head are covered in large green, brown or yellow scales. The avarge adult female weighs 50 kg and their carapace (shell) is approximately 80cm in lenght.",
+    shortDescription: "The hawksbill turtle is a critically endangered sea turtle found in tropical oceans. Known for it's striking, patterned shell."
+    }
+
+const perentie = {
+    lifespan: "20 years",
+    group: "reptile",
+    food: "Carnivore, they eat animals like kangaroos, rabbits, lizards and birds",
+    found: "deserts",
+    length: "2.5m",
+    weight: "20kg",
+    description: "The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia. It is one of the largest living lizards on earth, after the Komodo dragon, Asian water monitor, crocodile monitor, and intersecting by size with Nile monitor.[3] Found west of the Great Dividing Range in the arid areas of Australia, it is rarely seen, because of its shyness and the remoteness of much of its range from human habitation. The species is considered to be a least-concern species according to the International Union for Conservation of Nature. Its status in many Aboriginal cultures is evident in the totemic relationships, and part of the Ngiṉṯaka dreaming, as well as bush tucker. It was a favoured food item among desert Aboriginal tribes, and the fat was used for medicinal and ceremonial purposes.",
+    shortDescription: "The perentie is Australia's largest monitor lizard, found in arid and desert regions. Known for its speed and sharp claws, it is a skilled hunter."
+    };
