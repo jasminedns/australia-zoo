6e7e02681759b95b750bfcbc618d2209fc30cb9c
Moon2024-light <brite_moon2008@yahoo.com>
2024-11-30 13:51:41 +0100

    added variables again

--- scripts/reptiles.js
+++ scripts/reptiles.js
@@ -1,10 +1,1 @@
-// Ensure the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-    // Select the button
-    const button = document.getElementById("myButton");
-    
-    // Add a click event listener to the button
-    button.addEventListener("click", () => {
-        alert("Button was clicked!");
-    });
-});
+
