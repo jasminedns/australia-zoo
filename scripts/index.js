ffe8407c81cef1512bbb7836cf9d3d15649fe697
Moon2024-light <brite_moon2008@yahoo.com>
2024-11-30 14:16:29 +0100

    create menujs file

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,1 @@
-// Toggle side menu when hamburger is clicked
-const hamburgerMenu = document.getElementById('hamburgerMenu');
-const sideMenu = document.getElementById('sideMenu');
 
-hamburgerMenu.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-    hamburgerMenu.classList.toggle('open');
-});
