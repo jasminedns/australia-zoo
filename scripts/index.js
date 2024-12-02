eb94fab7139a8f5c0ae2944c263c7ec2765bc81f
idacarlin95 <carlin95@hotmail.se>
2024-12-03 00:03:52 +0100

    Added more info and images

--- scripts/index.js
+++ scripts/index.js
@@ -3,40 +3,40 @@ const animals = {
     echidna: {
         name: "Echidna",
         lifespan: "50 years",
-        group: "mammals",
+        group: "Mammals",
         food: "insects such as ants and termites, beetle larvae and worms",
         found: "Throughout Australia",
         description: "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
         shortDescription: "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs.",
         length: "76cm",
         weight: "10kg",
-        image: "../images/transparent-img/echidna.png"
+        image: "images/transparent-img/echidna.png"
     },
 
     tasmanianDevil: {
         name: "Tasmanian Devil",
         lifespan: "5 years", 
-        group: "mammals",
+        group: "Mammals",
         food: "A predator, then eat meat from other animals such as wallabies and wombats.",
         found: "Tasmania",
         description: "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina)[3] is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
         shortDescription: "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population.",
         length: "70cm",
         weight: "10kg",
-        image: "../image/transparent-img/tasmanian-devil.png."
+        image: "images/transparent-img/tasmanian-devil.png"
     },
 
     quokka: {
         name: "Quokka",
         lifespan: "10 years",
-        group: "mammals",
+        group: "Mammals",
         food: "Plant eaters, they munch on shrubs and grasses",
         found: "Only found on Rottnest Island and a few places on mainland Western Australia",
         description: "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws",
         shortDescription: "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'.",
         length: "50cm",
         weight: "3kg",
-        image: "../images/transparent-img/quokka.png"
+        image: "images/transparent-img/quokka.png"
     },
 
 //Repitiles
@@ -48,7 +48,7 @@ const animals = {
         found: "Northern Australia",
         length: "90cm",
         weight: "1kg",  
-        image: "'../images/transparent-img/frill-neck-lizard.png'",
+        image: "images/transparent-img/frill-neck-lizard.png",
         description: "When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles it's head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run wwthout stopping or looking back until reaches the safety of a tree.",
         shortDescription: "The frill-necked lizard is a reptile from northern Australia and New Guinea, known for its large, dramatic neck frill used to scare predators."
     },
@@ -61,7 +61,7 @@ const animals = {
         found: "Tropical coasts of Queensland, Northern Territory and Western Australia",
         length: "80cm (carapace)",
         weight: "50kg",
-        image: "'../images/transparent-img/hawkbill-turtle.png'",
+        image: "images/transparent-img/hawkbill-turtle.png",
         description: "The Hawksbill Turtle gets its common name from the distinctive mouth, which resembales a bird's beak. The shell, or carapace, is covered in large overlapping scutes, which are distinctive brown/green/amber background with lighter brown streaks throughout. At the edge of the carapace they overlap in such a way as to form a serrate edge. The turtle has an elongated head and flippers which have two visible claws on the end. The flippers and head are covered in large green, brown or yellow scales. The avarge adult female weighs 50 kg and their carapace (shell) is approximately 80cm in lenght.",
         shortDescription: "The hawksbill turtle is a critically endangered sea turtle found in tropical oceans. Known for it's striking, patterned shell."
     },
@@ -69,12 +69,12 @@ const animals = {
     perentie: {
         name: "Perentie",
         lifespan: "20 years",
-        group: "reptile",
+        group: "Reptiles",
         food: "Carnivore, they eat animals like kangaroos, rabbits, lizards and birds",
         found: "deserts",
         length: "2.5m",
         weight: "20kg",
-        image: "'../images/transparent-img/perentie.png'",
+        image: "images/transparent-img/perentie.png",
         description: "The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia. It is one of the largest living lizards on earth, after the Komodo dragon, Asian water monitor, crocodile monitor, and intersecting by size with Nile monitor.[3] Found west of the Great Dividing Range in the arid areas of Australia, it is rarely seen, because of its shyness and the remoteness of much of its range from human habitation. The species is considered to be a least-concern species according to the International Union for Conservation of Nature. Its status in many Aboriginal cultures is evident in the totemic relationships, and part of the Ngiṉṯaka dreaming, as well as bush tucker. It was a favoured food item among desert Aboriginal tribes, and the fat was used for medicinal and ceremonial purposes.",
         shortDescription: "The perentie is Australia's largest monitor lizard, found in arid and desert regions. Known for its speed and sharp claws, it is a skilled hunter."
     },
@@ -82,8 +82,8 @@ const animals = {
     cassowary: {
         name: "Cassowary",
         lifespan: "20 years",
-        group: "bird",
-        image: "../images/transparent-img/cassowary.png",
+        group: "Birds",
+        image: "images/transparent-img/cassowary.png",
         food: "Plants matter like fruit, insects and small animals like mice and lizards",
         shortDescription:   `It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. 
                             The largest cassowaries can stand as high as six feet and weigh up to 160 pounds. 
@@ -101,8 +101,8 @@ const animals = {
     kookaburra: {
         name: "Kookaburra",
         lifespan: "20 years",
-        group: "bird",
-        image: "../images/transparent-img/kookaburra.png",
+        group: "Birds",
+        image: "images/transparent-img/kookaburra.png",
         food: "Insects and small animals including snakes, frogs and lizards",
         shortDescription:   `Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest 
                             member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm 
@@ -121,8 +121,8 @@ const animals = {
     yellowTailedBlackCockatoo: {
         name: "Yellow-Tailed Black Cockatoo",
         lifespan: "41 years",
-        group: "bird",
-        image: "../images/transparent-img/yellow-tailed-black-cockatoo.PNG",
+        group: "Birds",
+        image: "images/transparent-img/yellow-tailed-black-cockatoo.PNG",
         food: "Fruit, seeds and other plant material",
         shortDescription:   `It has a short crest on the top of its head. Its plumage is mostly brownish black and 
                             it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged 
@@ -148,7 +148,12 @@ function updateContent(animal = null) {
 
     if (animal) {
         welcomeMessage.textContent = animal.name;
-        animalSummary.textContent = animal.shortDescription;
+        animalSummary.innerHTML = `
+            <img src="${animal.image}"; alt="Image of ${animal.group}" style="width: 200px; height: auto;"><br>
+            Decsription: ${animal.shortDescription}<br>
+            Food: ${animal.food}.<br>
+            Group: ${animal.group}.`;
+            
     } else {
         welcomeMessage.textContent = document.getElementById("welcome-message").dataset.defaultText;
         animalSummary.textContent = document.getElementById("animal-summary").dataset.defaultText;
