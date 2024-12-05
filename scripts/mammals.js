0ba7e15ffa59c0044b537f86ff499b513d35329e
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2024-12-05 22:48:19 +0100

    Added mammal clickevent

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -14,7 +14,7 @@ const addMammals = () => {
         lifespan: "5 years",
         group: "mammals",
         food: "A predator, then eat meat from other animals such as wallabies and wombats",
-        shortDescription: "",
+        shortDescription: "A carnivorous marsupial found in Tasmania, known for its loud screeches...",
         fullDescription: "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
         length: "70cm",
         weight: "10kg",
@@ -27,7 +27,7 @@ const addMammals = () => {
         lifespan: "10 years",
         group: "mammals",
         food: "Plant eaters, they munch on shrubs and grasses",
-        shortDescription: "",
+        shortDescription:  "A small, herbivorous marsupial known as the happiest animal due to its friendly appearance...",
         fullDescription: "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.",
         length: "50cm",
         weight: "3kg",
@@ -39,7 +39,7 @@ const addMammals = () => {
         lifespan: "50 years",
         group: "mammals",
         food: "insects such as ants and termites, beetle larvae and worms",
-        shortDescription: "",
+        shortDescription:  "A spiny, egg-laying mammal, one of only two monotremes in the world...",
         fullDescription: "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
         length: "76cm",
         weight: "10kg",
@@ -98,12 +98,12 @@ const buttonsAndEvents = () => {
                 readMoreEl.textContent = `Read more...`;
 
                 readMoreEl.addEventListener("click", () => {
-                    mammalDescEl.textContent = `${mammals[mammalButton].fullDescription}\n
-                 Weight:${mammals[mammalButton].weight}\n 
-                 Food:${mammals[mammalButton].food}\n 
-                 Lifespan:${mammals[mammalButton].lifespan}\n 
-                 Length:${mammals[mammalButton].length}\n
-                 Found:${mammals[mammalButton].found}`;
+                mammalDescEl.textContent = `${mammals[mammalButton].fullDescription}
+                 This mammal weight up to ${mammals[mammalButton].weight}.
+                 This mammal takes the food of ${mammals[mammalButton].food}.
+                 Mammal lifespan is ${mammals[mammalButton].lifespan}.
+                 The length of the mammal is ${mammals[mammalButton].length}.
+                 This mammal can be found in ${mammals[mammalButton].found}.`;
 
 
                     readMoreEl.textContent = ``;
