2d59ace35a7fd394fca6f496c9b88bab897346b6
DenVitaKaninen <tobiaskjernell@gmail.com>
2024-12-05 21:06:18 +0100

    added buttons and links refs

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,37 @@
+const pagesAndTitles = [
+    ["index.html", "home"],
+    ["birds.html", "birds"],
+    ["reptiles.html", "reptiles"],
+    ["mammals.html", "mammals"],
+    ["aboutus.html", "about us"]
+]
 // Toggle side menu when hamburger is clicked
 const hamburgerMenu = document.getElementById('hamburgerMenu');
 const sideMenu = document.getElementById('sideMenu');
 
+
+const generateMenuButtons = () => {
+    let button_parent = document.querySelector(".side-button-parent");
+
+    for (let index = 0; index < Object.keys(pagesAndTitles).length; index++) {
+        let button_li = document.createElement("li");
+        let button_link = document.createElement("a");
+
+        button_parent.appendChild(button_li);
+        button_li.appendChild(button_link);
+
+        let folderPath = window.location.pathname.split("/");
+
+        button_link.textContent = pagesAndTitles[index][1].toUpperCase();
+        console.log(folderPath[folderPath.length - 2]);
+        if (folderPath[folderPath.length - 2] !== "html")
+            button_link.href = index === 0 ? pagesAndTitles[index][0] : `html/${pagesAndTitles[index][0]}`;
+        else
+            button_link.href = index === 0 ? `../${pagesAndTitles[index][0]}` : pagesAndTitles[index][0];
+    }
+}
+
+//Events
 hamburgerMenu.addEventListener('click', () => {
     sideMenu.classList.toggle('open');
     hamburgerMenu.classList.toggle('open');
@@ -14,3 +44,5 @@ window.addEventListener('pageshow', (event) => {
         hamburgerMenu.classList.remove("open");
     }
 });
+
+generateMenuButtons();
