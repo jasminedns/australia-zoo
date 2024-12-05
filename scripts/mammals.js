c5d607310e302a96d347933c2729bfd4dc61c959
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2024-12-05 21:16:32 +0100

    Added changes

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -25,7 +25,7 @@
     }
 ];*/
 
-let allMammals = [];
+let mammals = [];
 let mammalMenuEl = document.querySelector(".animalmenu__container");
 let mammalFirstNameEl = document.querySelector(".section-2__title--changeContent-1");
 let mammalLastNameEl = document.querySelector(".section-2__title--changeContent-2");
@@ -34,23 +34,43 @@ let mammalDescEl = document.querySelector(".section-2__text--changeContent");
 let readMoreEl = document.querySelector(".section-2__text--readmore");
 
 const addMammals = () => {
-    allMammals.push({
+
+    mammals.push({
         name: "Tasmanian Devil",
-        shortDescription: "A carnivorous marsupial native to Tasmania, known for its screeches.",
-        fulldescription: "The Tasmanian Devil is the largest surviving carnivorous marsupial. They are nocturnal and have a strong bite, feeding on carrion, small animals, and insects.",
-        image: "tasmanian-devil.png"
+        image:"tasmanian-devil.png",
+        lifespan: "5 years",
+        group: "mammals",
+        food: "A predator, then eat meat from other animals such as wallabies and wombats",
+        shortDescription: "",
+        fullDescription: "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
+        length: "70cm",
+        weight: "10kg",
+        found: "Tasmania",
     });
-    allMammals.push({
-        name: "Quokka",
-        shortDescription: "A small herbivorous marsupial, often called the happiest animal.",
-        fulldescription: "The Quokka is a nocturnal animal from Rottnest Island, Australia, and is known for its smiling appearance. They are social and live in groups.",
-        image: "quokka.png"
+
+    mammals.push({
+        name: "quokka",
+        image : "quokka.png",
+        lifespan: "10 years",
+        group: "mammals",
+        food: "Plant eaters, they munch on shrubs and grasses",
+        shortDescription: "",
+        fullDescription: "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.",
+        length: "50cm",
+        weight: "3kg",
+        found: "Only found on Rottnest Island and a few places on mainland Western Australia",
     });
-    allMammals.push({
+    mammals.push({
         name: "Echidna",
-        shortDescription: "A spiny, egg-laying mammal, one of only two monotremes.",
-        fulldescription: "Echidnas are solitary animals found in Australia and New Guinea. They are known for their spines and long snouts, primarily eating ants and termites.",
-        image: "echidna.png"
+        image: "echidna.png",
+        lifespan: "50 years",
+        group: "mammals",
+        food: "insects such as ants and termites, beetle larvae and worms",
+        shortDescription: "",
+        fullDescription: "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
+        length: "76cm",
+        weight: "10kg",
+        found: "Throughout Australia",
     });
 }
 
@@ -69,11 +89,11 @@ const mammalSection2Content = () => {
 }
 
 const buttonsAndEvents = () => {
-    for (let mammalNumber = 0; mammalNumber < allMammals.length; mammalNumber++) {
-        let animalDiv = document.createElement("div");
-        animalDiv.textContent = allMammals[mammalNumber].name;
-        animalDiv.classList.add("animalMenu__element");
-        mammalMenuEl.appendChild(animalDiv);
+    for (let mammalNumber = 0; mammalNumber < mammals.length; mammalNumber++) {
+        let buttonAnimal = document.createElement("div");
+        buttonAnimal.textContent = mammals[mammalNumber].name;
+        buttonAnimal.classList.add("animalMenu__element");
+        mammalMenuEl.appendChild(buttonAnimal);
 
         let animalMenuEls = document.querySelectorAll(".animalMenu__element");
 
@@ -83,35 +103,41 @@ const buttonsAndEvents = () => {
                 item.classList.add("animalMenu__element--active");
 
                 // Set Mammal Details
-                let splitName = allMammals[mammalButton].name.split(" ");
+                let splitName = mammals[mammalButton].name.split(" ");
 
-                mammalFirstNameEl.textContent = splitName[0];
-                mammalLastNameEl.textContent = splitName[1] || ''; 
-                mammalLastNameEl.classList.remove("hidden");
-                mammalFirstNameEl.classList.add("section-2__title--active");
-
+                if (mammalButton === 1) {
+                    let splitName = mammals[mammalButton].name.split(" ");
+                    mammalFirstNameEl.textContent = splitName[0];
+                    mammalLastNameEl.textContent = splitName[1] || '';
+                    mammalLastNameEl.classList.remove("hidden");
+                    mammalFirstNameEl.classList.add("section-2__title--active");
+                } else {
+                    mammalFirstNameEl.textContent = mammals[mammalButton].name;
+                    mammalLastNameEl.textContent = ``;
+                    mammalLastNameEl.classList.add("hidden");
+                }
                 mammalImgEl.classList.remove("hidden");
-                mammalImgEl.src = `../images/transparent-img/${allMammals[mammalButton].image}`;
-                mammalImgEl.alt = `Photo of ${allMammals[mammalButton].name}`;
+                mammalImgEl.src = `../images/transparent-img/${mammals[mammalButton].image}`
+                mammalImgEl.alt = `Photo of ${mammals[mammalButton].name}`;
 
-                mammalDescEl.textContent = allMammals[mammalButton].shortDescription;
+                mammalDescEl.textContent = mammals[mammalButton].shortDescription;
                 readMoreEl.classList.remove("hidden");
                 readMoreEl.textContent = `Read more...`;
-            });
-
-            // Read More functionality
-            readMoreEl.addEventListener("click", () => {
-                mammalDescEl.textContent = allMammals[mammalButton].fulldescription;
-                readMoreEl.textContent = `Read Less...`;
-
                 readMoreEl.addEventListener("click", () => {
-                    mammalDescEl.textContent = allMammals[mammalButton].shortDescription;
-                    readMoreEl.textContent = `Read more...`;
+                mammalDescEl.textContent =  `${mammals[mammalButton].fullDescription}\n Weight:${mammals[mammalButton].weight}\n Food:${mammals[mammalButton].food}\n Lifespan:${mammals[mammalButton].lifespan}\n Length:${mammals[mammalButton].length}\n Found:${mammals[mammalButton].found}`,
+       
+        
+                readMoreEl.textContent = ``;
+    
+                    // readMoreEl.addEventListener("click", () => {
+                    //     mammalDescEl.textContent = allMammals[mammalButton].shortDescription;
+                    //     readMoreEl.textContent = `Read more...`;
                 });
             });
         });
+
     }
-}
+};
 
 const launch = () => {
     addMammals();
